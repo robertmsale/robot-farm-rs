a91572caf37fac6873b859265744da2daf60015d
Robert Sale <robertmsale@icloud.com>
2025-11-21 11:03:10 -0800

    Added docker overrides

---
+++ openapi/flutter_client/doc/DockerOverrides.md
@@ -0,0 +1,17 @@
+# my_api_client.model.DockerOverrides
+
+## Load the model package
+```dart
+import 'package:my_api_client/api.dart';
+```
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**orchestrator** | **List<String>** |  | [default to const []]
+**worker** | **List<String>** |  | [default to const []]
+**wizard** | **List<String>** |  | [default to const []]
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ openapi/flutter_client/test/docker_overrides_test.dart
@@ -0,0 +1,37 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.18
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:my_api_client/api.dart';
+import 'package:test/test.dart';
+
+// tests for DockerOverrides
+void main() {
+  // final instance = DockerOverrides();
+
+  group('test DockerOverrides', () {
+    // List<String> orchestrator (default value: const [])
+    test('to test the property `orchestrator`', () async {
+      // TODO
+    });
+
+    // List<String> worker (default value: const [])
+    test('to test the property `worker`', () async {
+      // TODO
+    });
+
+    // List<String> wizard (default value: const [])
+    test('to test the property `wizard`', () async {
+      // TODO
+    });
+
+
+  });
+
+}
